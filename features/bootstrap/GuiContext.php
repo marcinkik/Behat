6fb6f544ffd516c43fe92271aff02466cfee3ce4
MarcinK <kiklica@o2.pl>
2015-10-20 23:28:50 +0200

    Create property configuration

---
+++ features/bootstrap/GuiContext.php
@@ -0,0 +1,25 @@
+<?php
+use Behat\Behat\Context\BehatContext;
+
+use Behat\Mink\Mink,
+    Behat\Mink\Session,
+    Behat\Mink\Driver\Selenium2Driver;
+
+use Selenium\Client as SeleniumClient;
+
+require_once 'PHPUnit/Autoload.php';
+require_once 'PHPUnit/Framework/Assert/Functions.php';
+
+class GuiContext extends BehatContext
+{
+
+    public function __construct(array $parameters)
+    {
+        $mink = new Mink(array(
+            'selenium2' => new Session(new Selenium2Driver($parameters['wd_capabilities']['browser'], $parameters['wd_capabilities'], $parameters['wd_host'])),
+        ));
+
+        $this->gui = $mink->getSession('selenium2');
+    }
+
+}
