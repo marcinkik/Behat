2c8f6a8957e2c457ef26a7cb314df1334265d29d
MarcinK <kiklica@o2.pl>
2015-10-21 00:17:41 +0200

    EOD. End of day.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -5,10 +5,13 @@ use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
+
+
+
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
+class FeatureContext implements Context, SnippetAcceptingContext
 {
     /**
      * Initializes context.
@@ -20,5 +23,24 @@ class FeatureContext extends MinkContext implements Context, SnippetAcceptingCon
     public function __construct()
     {
 
+    }
+
+    public function iAmOn($path){
+        $this->gui->start();
+        $this->gui->visit($path);
+        $this->gui->wait(10000);
+        $this->gui->stop();
+    }
+
+    public function iSearchFor($text){
+        $page = $this->gui->getPage();
+        $page->fillField("lst-ib",$text);
+        $page->find("css",".jsb input[name=btnk']")->click();
+    }
+
+    public function iShouldSeeAsTheFirstResult($goal){
+        $text = $this->gui->getPage()->find('css',"h3 a")->getText();
+        assert($text, $goal);
+        $this->gui->stop();
     }
 }
