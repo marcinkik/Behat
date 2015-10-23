efdd14fc40c0c38d5c614139447b01ffb74a2ef5
Marcin Kiklica <marcin.kiklica@rst.com.pl>
2015-10-23 14:35:18 +0200

    add fake tests and mini handler bugs

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -5,6 +5,8 @@ use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
+use Behat\Testwork\Hook\Scope\AfterSuiteScope;
+use Behat\Testwork\Tester\Result\TestResults;
 
 
 use Behat\Mink\Mink,
@@ -32,6 +34,24 @@ class FeatureContext implements Context, SnippetAcceptingContext
     {
         $mink = new Mink(array('selenium2' => new Session(new Selenium2Driver($browser, null, $wd_host))));
         $this->gui = $mink->getSession('selenium2');
+    }
+    /**
+     * @AfterStep
+     */
+//    public function printRequestAndResponseWhenStepWasFailed(\Behat\Behat\Hook\Scope\AfterStepScope $scope)
+//    {
+//        if (TestResults::FAILED === $scope->getTestResult()->getResultCode()) {
+//            $this->printDebug($scope->getTestResult()->getResultCode());
+//        }
+//    }
+    /**
+     * Prints debug string.
+     *
+     * @param string $string debug string
+     */
+    public function printDebug($string)
+    {
+        echo "\n\033[36m|  " . strtr($string, ["\n" => "\n|  "]) . "\033[0m\n\n";
     }
     /**
      * @Given I am on :web
@@ -72,10 +92,21 @@ class FeatureContext implements Context, SnippetAcceptingContext
      */
     public function iShouldSee($message_text)
     {
-        //$this->gui->wait(1000);
+        $this->gui->wait(1000);
         $page = $this->gui->getPage();
-        $text = $page->find("named", array("content", $message_text))->getText();
-        assertEquals($message_text, $text);
+        //$text = $page->find("named", array("content", $message_text))->getText();
+        //$text = $page->find("named", array("content", $message_text));
+        $text = $page->find('css', '.message-wrapper')->getText();
+        assertEquals($message_text,$text);
+        //var_dump($text2);
+        //$result = assertEquals("x","y");
+        //var_dump(assertEquals("x","y"));
+//        if($text == null){
+//            throw new \Exception('The element is not found');
+//        }
+//        else{
+//            assertEquals($message_text, $text->getText());
+//        }
 //        if($text != null){
 //            assertEquals($message_text, $text->getText());
 //        }
--- features/web/LoginPage.feature
+++ features/web/LoginPage.feature
@@ -2,14 +2,21 @@
 
 Feature: Login page works correctly and handle errors message
 
-  Scenario: Log in with empty password causes error message shows
+  Scenario: FAKE Log in with empty password causes error message shows
     Given I am on "http://trans-inkasso-web.rc-trans.rst.com.pl/user-panel.html#/cases"
      And I set "51-27" in "TransId"
     When I press "Log in" button
-    Then I should see "Both fields are required."
+    Then I should see "Both fields are required.."
 
-  Scenario: FAKE Log in with empty password causes error message shows
+  Scenario:  Log in with empty password causes error message shows
     Given I am on "http://trans-inkasso-web.rc-trans.rst.com.pl/user-panel.html#/cases"
     And I set "51-27" in "TransId"
     When I press "Log in" button
-    Then I should see "Both fields are required.."
+    Then I should see "Both fields are required."
+
+   Scenario: Log in with incorrect password causes display error message
+     Given I am on "http://trans-inkasso-web.rc-trans.rst.com.pl/user-panel.html#/cases"
+     And I set "51-27" in "TransId"
+     And I set "incorrect password" in "Password"
+     When I press "Log in" button
+     Then I should see "Invalid credentials"
